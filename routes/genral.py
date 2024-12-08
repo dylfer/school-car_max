5b032f5d9352d7e8b818caa9f587b505cb3f7ec8
dylan <ferrow.dylan.code@gmail.com>
2024-12-08 20:50:07 +0000

    wip: base templates

--- routes/genral.py
+++ routes/genral.py
@@ -2,40 +2,42 @@ from flask import Blueprint, render_template
 
 genral_router = Blueprint('genral', __name__)
 
+# TODO add titles as page pramiters
+
 
 @genral_router.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('base.html', content_template='index.html')
 
 
 @genral_router.route('/login')
 def login():
-    return render_template('login.html')
+    return render_template('base.html', content_template='login.html')
 
 
 @genral_router.route('/signup')
 def signup():
-    return render_template('signup.html')
+    return render_template('base.html', content_template='signup.html')
 
 
 @genral_router.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('base.html', content_template='about.html')
 
 
 @genral_router.route('/contact')
 def contact():
-    return render_template('contact.html')
+    return render_template('base.html', content_template='contact.html')
 
 
 @genral_router.route('/termsandconditions')
 def termsandconditions():
-    return render_template('terms.html')
+    return render_template('base.html', content_template='terms.html')
 
 
 @genral_router.route('/privacy')
 def privacy():
-    return render_template('privacy.html')
+    return render_template('base.html', content_template='privacy.html')
 
 
 # customers
@@ -43,19 +45,19 @@ def privacy():
 
 @genral_router.route("/serch")
 def serch():
-    return render_template('serch.html')
+    return render_template('base.html', content_template='serch.html')
 
 
 @genral_router.route("/bassket")
 def bassket():
-    return render_template('bassket.html')
+    return render_template('base.html', content_template='bassket.html')
 
 
 @genral_router.route("/order")
 def order():
-    return render_template('order.html')
+    return render_template('base.html', content_template='order.html')
 
 
 @genral_router.route("/payment")
 def payment():
-    return render_template('pay.html')
+    return render_template('base.html', content_template='pay.html')
